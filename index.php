77b3ff85e3423f6de1881d739273ac3ee768dabd
milendd <milen_d@abv.bg>
2014-08-22 16:28:45 +0300

    Add input field for the content

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Team "Spike" - Forum</title>
+    <script src="scripts/jquery-1.11.1.min.js"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles/main.css">
+</head>
+<body>
+    <div id="wrapper">
+        <form method="post">
+            <input type="text" name="title" placeholder="title">
+            <textarea name="content"></textarea>
+            <input type="submit" value="Add new theme" id="addNewTheme">
+        </form>
+        <?php
+        if(isset($_POST['title']) && isset($_POST['content'])){
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+
+            echo "<div class=\"article\">";
+            echo "<h4>$title</h4>";
+            echo "<div>$content</div>";
+            echo "</div>";
+        }
+        ?>
+    </div>
+    <script src="scripts/script.js"></script>
+</body>
+</html>
