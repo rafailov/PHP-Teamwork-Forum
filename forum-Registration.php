f0069eb051fdc8c803a43a54eb71776978d6e9a3
Magdalena Petrova <magik70@abv.bg>
2014-08-23 16:32:57 +0300

    magdalena70

---
+++ forum-Registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<tiltle>Team "Spike" - Forum-Registration</title>
+		<link rel="stylesheet" href="styles/forum-Registration.css">
+	</head>
+	<body>
+		<header>
+		
+		</header>
+		<section>
+			<form action="" method="get">
+				Name:<input type="text" name="name"><br>
+				E-mail:<input type="text" name="email"><br>
+				<input type="submit">
+			</form>
+			<?php
+			if(isset($_GET["name"])) {
+			?>
+			Welcome
+			<?php
+			echo htmlentities($_GET["NAME"])
+			?><br>
+			Your email is:<?php echo htmlentities(_GET["email"]) ?>
+			<?php } ?>
+		</section>
+		<footer>
+		
+		</footer>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,30 +1,49 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Team "Spike" - Forum</title>
-    <script src="scripts/jquery-1.11.1.min.js"></script>
-    <script src="scripts/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="styles/main.css">
-</head>
-<body>
-    <div id="wrapper">
-        <form method="post">
-            <input type="text" name="title" placeholder="title">
-            <textarea name="content"></textarea>
-            <input type="submit" value="Add new theme" id="addNewTheme">
-        </form>
-        <?php
-        if(isset($_POST['title']) && isset($_POST['content'])){
-            $title = $_POST['title'];
-            $content = $_POST['content'];
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Team "Spike" - Forum</title>
+		<script src="scripts/jquery-1.11.1.min.js"></script>
+		<script src="scripts/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="styles/main.css">
+	</head>
+	<body>
+		<header>
+			<h1>
+				<a href="#"><img src="img/logo.jpg" name="logo" alt="logo"/>FORUM.BG</a><p>Welcome to Forum.BG</p>
+			</h1>
+			<form>
+				<a href="#forum-Registration.php"><input type="submit" value="registration" id="registration"></a>
+				<a href="#"><input type="submit" value="login" id="login"></a>
+			</form>	
+		</header>
+		
+		<section>
+			<div id="wrapper">
+				<form method="post">
+				<input type="text" name="title" placeholder="Title" id="title">
+				<textarea name="content"></textarea>
+				<input type="submit" value="Add new theme" id="addNewTheme">
+				</form>
+				<?php
+				if(isset($_POST['title']) && isset($_POST['content'])){
+					$title = $_POST['title'];
+					"<br><br>";
+					$content = $_POST['content'];
 
-            echo "<div class=\"article\">";
-            echo "<h4>$title</h4>";
-            echo "<div>$content</div>";
-            echo "</div>";
-        }
-        ?>
-    </div>
+					echo "<div class=\"article\">";
+					echo "<h4>$title</h4>";
+					echo "<div>$content</div>";
+					echo "</div>";
+				}
+				?>
+			</div>
+		</section>
+		
+		<footer>
+			<address>Bulgaria Sofia 1510 tel:+359 0888123456</address>
+		</footer>
+
     <script src="scripts/script.js"></script>
 </body>
 </html>
