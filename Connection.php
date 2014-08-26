37933794d1ecf1e843b646fe11675e221ee2c2eb
Denis <alarma7a@gmail.com>
2014-08-26 10:02:26 +0300

    add database Connection.php

---
+++ Connection.php
@@ -0,0 +1,56 @@
+<?php
+class DatabaseConnect {
+
+    public $_db;
+    /*
+     * connect database when create new instance
+     */
+    public function __construct() {
+
+        $db = new mysqli('localhost','', '', '');
+        if ($db->connect_errno) {
+            header('Location: errorPages' . DS . 'database.php');
+            exit;
+        } else {
+            $this->_db = $db;
+        }
+    }
+    /*
+     * this function execute sql
+     */
+    public function execute($sql) {
+
+        $this->_db->query('SET NAMES utf8');
+        $query = $this->_db->query($sql);
+        if (!$query) {
+            echo $this->_db->error;
+        } else {
+            return $query;
+        }
+    }
+    /*
+     * this function escape string (for sql injection)
+     */
+    public function escape($string) {
+
+        $escaped = $this->_db->real_escape_string($string);
+
+        return $escaped;
+    }
+
+    /*
+     * close connection
+     */
+    public function __destruct() {
+        $this->_db->close();
+    }
+    /*
+     * this function tell us next insert id 
+     * 
+     */
+    public function inserted_id() {
+        return $this->_db->insert_id;
+    }
+    
+
+}
